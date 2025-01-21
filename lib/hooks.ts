b603f3d971b9e987417230284c78cc25ff43521e
BenMukebo <l.benkasmukebo7@gmail.com>
2025-01-21 10:53:56 +0200

    Add new hooks

--- lib/hooks.ts
+++ lib/hooks.ts
@@ -3,21 +3,25 @@ import useSWR from "swr";
 
 const supabase = createClient();
 
-export function useTransactions(accountId: string) {
-  return useSWR(["transactions", accountId], async () => {
+export function useUserProfile(userId: string) {
+  return useSWR(["userProfile", userId], async () => {
     const { data, error } = await supabase
-      .from("transactions")
-      .select(
-        `
-        *,
-        categories (
-          id,
-          name,
-          parent_id
-        )
-      `
-      )
-      .eq("account_id", accountId);
+      .from("profiles")
+      .select("*")
+      .eq("id", userId)
+      .single();
+
+    if (error) throw error;
+    return data;
+  });
+}
+
+export function useAccounts(userId: string) {
+  return useSWR(["accounts", userId], async () => {
+    const { data, error } = await supabase
+      .from("accounts")
+      .select("*")
+      .eq("user_id", userId);
 
     if (error) throw error;
     return data || [];
@@ -37,9 +41,31 @@ export function useAccount(accountId: string) {
   });
 }
 
+
 export function useCategories() {
   return useSWR("categories", async () => {
     const { data, error } = await supabase.from("categories").select("*");
+
+    if (error) throw error;
+    return data || [];
+  });
+}
+
+export function useTransactions(accountId: string) {
+  return useSWR(["transactions", accountId], async () => {
+    const { data, error } = await supabase
+      .from("transactions")
+      .select(
+        `
+        *,
+        categories (
+          id,
+          name,
+          parent_id
+        )
+      `
+      )
+      .eq("account_id", accountId);
 
     if (error) throw error;
     return data || [];
